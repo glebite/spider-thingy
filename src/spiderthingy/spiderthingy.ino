53d392aa935b8540d0e9e43bd7c377eef5eabeb7
unknown <glebite@gmail.com>
2021-09-19 22:26:39 -0400

    Updating...

--- src/spiderthingy/spiderthingy.ino
+++ src/spiderthingy/spiderthingy.ino
@@ -1,11 +1,12 @@
 #include <ServoEasing.h>
-#include "PinDefinitionsAndMore.h"
+// #include "PinDefinitionsAndMore.h"
 
 // spider-thingy
 // 4 legged spider based of of countless other ideas
 ServoEasing Servo1;
 ServoEasing Servo2;
 
+uint16_t sServoSpeed = 90;
 
 #define START_DEGREE_VALUE 0
 
